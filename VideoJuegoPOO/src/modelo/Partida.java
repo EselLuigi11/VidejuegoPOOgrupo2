449774e9c91d225c91ac7fe005f2b926e3f00134
Ignacioho07 <137006133+Ignacioho07@users.noreply.github.com>
2026-05-25 12:36:14 -0300

    Update Partida.java

--- VideoJuegoPOO/src/modelo/Partida.java
+++ VideoJuegoPOO/src/modelo/Partida.java
@@ -1,5 +1,25 @@
 package modelo;
 
 public class Partida {
-
+	private int nivel;
+	private boolean estado;
+	private Party grupo;
+	
+	public Partida() {
+		this.nivel = 1;
+		this.estado = true;
+		this.grupo = new Party("Los vengadores");
+	}
+	
+	public void pasarSiguienteNivel() {
+		this.nivel++;
+		System.out.println("Avanzaste al nivel: " + this.nivel + "!");
+	}
+	
+	public void verificarEstadoPartida() {
+		if (this.grupo.estaVivo() == false) {
+			this.estado = false;
+			System.out.println("Game Over");
+		}
+	}
 }
