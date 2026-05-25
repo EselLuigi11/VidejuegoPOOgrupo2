02bc40e59ef71aedb95be65b3d21be552d9889af
Ignacioho07 <137006133+Ignacioho07@users.noreply.github.com>
2026-05-25 12:36:42 -0300

    Update Turno.java

--- VideoJuegoPOO/src/modelo/Turno.java
+++ VideoJuegoPOO/src/modelo/Turno.java
@@ -1,5 +1,17 @@
 package modelo;
 
 public class Turno {
-
+	// 1. Atributos
+	private int numeroDeTurno;
+	private Entidad personajeActual;
+	
+	// 2. Constructor
+	public Turno (int numeroDeTurno, Entidad personajeActual) {
+		this.numeroDeTurno = numeroDeTurno;
+		this.personajeActual = personajeActual;
+	}
+	
+	public void procesarAccion (Accion accionElegida) {
+		accionElegida.ejecutar();
+	}
 }
