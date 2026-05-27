62d12d230d7856ab5b38a580c063824398694f93
JMsamuraiX4 <juanmartinramosgr@gmail.com>
2026-05-27 00:07:22 -0300

    Update Habilidad.java

--- VideoJuegoPOO/src/modelo/Habilidad.java
+++ VideoJuegoPOO/src/modelo/Habilidad.java
@@ -1,5 +1,17 @@
-package modelo;
+public class HabilidadEsp implements Accion {
+    private Entidad usuario;
+    private Entidad objetivo;
+    private String nombreHabilidad;
 
-public class Habilidad extends Accion {
+    public HabilidadEsp(Entidad usuario, Entidad objetivo, String nombreHabilidad) {
+        this.usuario = usuario;
+        this.objetivo = objetivo;
+        this.nombreHabilidad = nombreHabilidad;
+    }
 
+    @Override
+    public void ejecutar() {
+        System.out.println("🔥 " + usuario.getNombre() + " usa su habilidad especial: " + nombreHabilidad + " en " + objetivo.getNombre());
+        // Lógica de la habilidad especial
+    }
 }
