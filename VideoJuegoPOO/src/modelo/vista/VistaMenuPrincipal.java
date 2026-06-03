45fbb50d53d2597a2941d5ab08d24f265d60474f
JMsamuraiX4 <juanmartinramosgr@gmail.com>
2026-06-03 07:55:47 -0300

    Add background image and update button styles

--- VideoJuegoPOO/src/modelo/vista/VistaMenuPrincipal.java
+++ VideoJuegoPOO/src/modelo/vista/VistaMenuPrincipal.java
@@ -3,7 +3,10 @@ package modelo.vista;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 import java.awt.GridLayout;
+import java.awt.BorderLayout;
 
 public class VistaMenuPrincipal extends JFrame {
     
@@ -17,17 +20,31 @@ public class VistaMenuPrincipal extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+        JLabel fondo = new JLabel(new ImageIcon(getClass().getResource("/img/fondo_menu.png")));
+        fondo.setLayout(new BorderLayout());
+        setContentPane(fondo);
+
         JPanel panelBotones = new JPanel();
         panelBotones.setLayout(new GridLayout(3, 1, 10, 10));
+        panelBotones.setOpaque(false);
 
         btnNuevaPartida = new JButton("Nueva Partida");
         btnCargar = new JButton("Cargar");
         btnSalir = new JButton("Salir");
 
+        btnNuevaPartida.setOpaque(false);
+        btnNuevaPartida.setContentAreaFilled(false);
+        
+        btnCargar.setOpaque(false);
+        btnCargar.setContentAreaFilled(false);
+        
+        btnSalir.setOpaque(false);
+        btnSalir.setContentAreaFilled(false);
+
         panelBotones.add(btnNuevaPartida);
         panelBotones.add(btnCargar);
         panelBotones.add(btnSalir);
 
-        add(panelBotones);
+        fondo.add(panelBotones, BorderLayout.CENTER);
     }
 }
