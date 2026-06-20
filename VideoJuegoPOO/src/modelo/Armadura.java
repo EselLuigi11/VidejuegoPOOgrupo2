67ffa30e2be0d4f1dd9636f98937bc88518c901b
Ignacioho07 <137006133+Ignacioho07@users.noreply.github.com>
2026-06-20 19:20:25 -0300

    Update Armadura.java

--- VideoJuegoPOO/src/modelo/Armadura.java
+++ VideoJuegoPOO/src/modelo/Armadura.java
@@ -5,34 +5,25 @@ import modelo.entidades.Heroe;
 public class Armadura extends Item {
 	private static final long serialVersionUID = 1L;
 	private int plusDefensa;
-	
+
 	public Armadura(String nombre, String descripcion, int defensa) {
 		super(nombre, descripcion);
 		this.plusDefensa = defensa;
-		
-	}
-	
-	
-	public void usar(Heroe heroe) {
-		heroe.aumentarDefensa(plusDefensa);
-		System.out.println("Armadura equipada");
-		
 	}
 
+	public void usar(Heroe heroe) {
+		heroe.equiparArmadura(this);
+	}
 
 	public int getplusDefensa() {
 		return plusDefensa;
 	}
 
-
 	public void setDefensa(int plusDefensa) {
 		this.plusDefensa = plusDefensa;
 	}
-	
-	
+
 	public String toString() {
-		return super.toString() + 
-				"| Defensa: " +
-				plusDefensa;
+		return super.toString() + "| Defensa: " + plusDefensa;
 	}
 }
